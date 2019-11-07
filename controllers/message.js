0cff73d4b4db61775a9ac6c9fb315ddc3bf04573
Hassan <hassan_saleem@outlook.com>
2019-11-08 00:23:21 +0500

    changes

--- controllers/message.js
+++ controllers/message.js
@@ -1,4 +1,4 @@
-var Message = require('../models/message.model')
+// var Message = require('../models/message.model')
 
 module.exports = {
 	get: function (request, response) {
@@ -12,11 +12,7 @@ module.exports = {
 
 	post: function (req, res) {
 		// save messages ti db.
-		console.log(req)
-		console.log(req.body) // To display incomming request in the console
-		var message = new Message(req.body) // storing the incomming message in a the dataBase
-
-		message.save()
-		res.status(200)
+		
+		// res.status(200)
 	}
 }
